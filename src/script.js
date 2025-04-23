97509bd153f404ab2264f2d4d35cb3b849ae8410
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2025-04-23 14:03:20 +0300

    feat: add footer

--- src/script.js
+++ src/script.js
@@ -1,11 +1,17 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+
 menuOpenButton.addEventListener("click", () => {
   document.body.classList.toggle("show-mobile-menu");
 });
 
 menuCloseButton.addEventListener("click", () => {
   menuOpenButton.click();
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
 });
 
 const swiper = new Swiper(".slider-wrapper", {
